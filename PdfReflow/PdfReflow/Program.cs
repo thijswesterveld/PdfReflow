0f82b9ccf773344d959e277e81a4237c242e7c86
thijswesterveld <thijs.westerveld@acm.org>
2014-06-02 16:24:39 +0200

    Create training collection from metadata file

--- PdfReflow/PdfReflow/Program.cs
+++ PdfReflow/PdfReflow/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,36 @@ namespace PdfReflow
     {
         static void Main(string[] args)
         {
+            string indexFile = @"C:\Users\ThijsWizeNoze\Documents\pdfhtmltest\Metadata pdfs_TM.csv";
             string inputDir = @"C:\Users\ThijsWizeNoze\Documents\pdfhtmltest\bboxhtml\";
-            string outputDir = @"C:\Users\ThijsWizeNoze\Documents\pdfhtmltest\txt_mergereorder\";
-            foreach (string file in System.IO.Directory.EnumerateFiles(inputDir, "*.html"))
+            string outputDir = @"C:\Users\ThijsWizeNoze\Documents\pdfhtmltest\trainingData\";
+
+            TextReader tr = new StreamReader(indexFile);
+            string line = tr.ReadLine();
+            while(line != null)
             {
-                Console.WriteLine(file);
-                string baseName = System.IO.Path.GetFileNameWithoutExtension(file);
-                PdfTextDocument doc = new PdfTextDocument();
-                doc.FromXHtmlFile(file);
-                doc.Reflow();
-                doc.Export(System.IO.Path.Combine(outputDir,baseName), baseName);
+                Console.WriteLine(line);
+                string[] fields = line.Split(',');
+                string isbn = fields[0];
+                string title = fields[1];
+                string group = fields[2];
+                string subject = fields[3];
+                string type =fields[4];
+
+                foreach (string file in System.IO.Directory.EnumerateFiles(inputDir, isbn + "*.html"))
+                {
+                    string baseName = isbn + "_" + subject + "_" + type;
+                    PdfTextDocument doc = new PdfTextDocument();
+                    doc.FromXHtmlFile(file);
+                    doc.Reflow();
+                    string path = System.IO.Path.Combine(outputDir, "groep"+group);
+                    if(!System.IO.Directory.Exists(path))
+                    {
+                        System.IO.Directory.CreateDirectory(path);
+                    }
+                    doc.Export(path, baseName);
+                }
+                line = tr.ReadLine();
             }
             Console.Read();
         }
